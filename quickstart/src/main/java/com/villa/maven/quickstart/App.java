b71f528f6197d64fb5514c9f8e7d36d6c0e1928d
Fabio <Fabio@192.168.1.114>
2020-01-04 13:15:43 +0100

    scrittura classe App

--- quickstart/src/main/java/com/villa/maven/quickstart/App.java
+++ quickstart/src/main/java/com/villa/maven/quickstart/App.java
@@ -1,13 +1,12 @@
 package com.villa.maven.quickstart;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.gson.Gson;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
     }
 }
